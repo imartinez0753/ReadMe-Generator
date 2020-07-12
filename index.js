3de70a9235747eae4f507cce8694ddce0a4f7e30
Ian Martinez <imartinez0753@gmail.com>
2020-07-12 14:58:03 -0700

    I have started making objects and assigning them questions from an array

---
+++ index.js
@@ -0,0 +1,74 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+function promptUser() {
+  return requirer.prompt([
+    {
+      type: "input",
+      name: "project",
+      message: questions[0]
+    },
+    {
+      type: "input",
+      name: "description",
+      message: questions[1]
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: questions[2]
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: questions[3]
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: questions[4]
+    },
+    {
+      type: "input",
+      name: "test",
+      message: questions[5]
+    },
+    {
+      type: "input",
+      name: "license",
+      message: questions[6]
+    },
+    {
+      type: "input",
+      name: "gitHub",
+      message: questions[7]
+    },
+    {
+      type: "input",
+      name: "email",
+      message: questions[8]
+    }
+  ]);
+}
+
+// array of questions for user
+const questions = [
+  "What is the Project Title?",
+  "Please enter a description of project",
+  "enter installation instructions",
+  "Please enter usage infromation",
+  "Please enter contribution guidelines",
+  "Please enter test instructions",
+  "Please enter license information",
+  "Please enter Github username",
+  "Please eneter your e-mail"
+];
+
+// function to write README file
+function writeToFile(fileName, data) {}
+
+// function to initialize program
+function init() {}
+
+// function call to initialize program
+init();
